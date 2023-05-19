cebb75db1af067f281c2aad5697ce0e668323282
kamil <68158507+kamilkej@users.noreply.github.com>
2023-05-19 20:11:44 +0200

    Add files via upload

---
+++ joomla_zsegw/index.php
@@ -0,0 +1,154 @@
+<?php defined('_JEXEC') or die('Restricted access');?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <meta charset="UTF-8" />
+  <title>ZSE Mockup</title>
+  <link rel="stylesheet" type="text/css" href="css/template.css" />
+</head>
+<body>
+<header>
+<jdoc:include type="header" />
+	<div id="title"></div> 
+	<div id="header_text"></div>  
+</header>
+	<nav class="hdr">
+    <jdoc:include type="modules" name="menu" /> 
+		<h3>Menu witryny</h3>
+<ul class="menu"><li id="current" class="active item1"><a href="http://zsegw.pl/"><span>Start</span></a></li><li class="item104"><a href="/index.php?option=com_content&amp;view=article&amp;id=105&amp;Itemid=104"><span>Kontakt</span></a></li><li class="item2"><a href="/index.php?option=com_content&amp;view=category&amp;id=38&amp;Itemid=2"><span>Rekrutacja</span></a></li><li class="item211"><a href="http://internat.zsegw.pl"><span>Internat</span></a></li><li class="item134"><a href="/index.php?option=com_content&amp;view=section&amp;id=10&amp;Itemid=134"><span>Archiwum</span></a></li><li class="item50"><a href="https://zselgorzow.bip.gov.pl/" target="_blank"><span>BIP ZSEL</span></a></li><li class="item224"><a href="/index.php?option=com_content&amp;view=category&amp;layout=blog&amp;id=65&amp;Itemid=224"><span>KSZTAŁCENIE NA ODLEGŁOŚĆ</span></a></li><li class="item228"><a href="http://zsegw.pl/index.php?option=com_content&amp;view=article&amp;id=1052:covid-19&amp;catid=39:o-szkole"><span>COVID-19</span></a></li><li class="item229"><a href="/index.php?option=com_content&amp;view=article&amp;id=1053&amp;Itemid=229"><span>mLegitymacja</span></a></li></ul>
+	</nav>
+<section id="slider">
+	<div class="wrapper">
+		<ul id="sb-slider" class="sb-slider" style="text-align: center; max-width: 600px; overflow: hidden;">
+			<li style="display: none;" class="">
+				<a href="http://zsegw.pl" target="_blank"><img src="http://zsegw.pl/images/slider/slider2021.jpg" alt="image1" style="width: 600px; height: 376px;"></a>
+				<div class="sb-description">
+					<h3>Nowe kierunki!</h3>
+				</div>
+			</li>
+			<li style="display: none;" class="">
+				<a href="http://zsegw.pl/index.php?option=com_content&amp;view=article&amp;id=1026&amp;Itemid=223" target="_blank"><img src="http://zsegw.pl/images/monter_sieci.jpg" alt="image2"></a>
+				<div class="sb-description">
+					<h3>Monter sieci i urządzeń telekomunikacyjnych</h3>
+				</div>
+			</li>
+			<li style="display: none;" class="">
+				<a href="http://zsegw.pl/programista/index.html" target="_blank"><img src="http://zsegw.pl/images/programista.jpg" alt="image1"></a>
+				<div class="sb-description">
+					<h3>Technik Programista</h3>
+				</div>
+			</li>
+			<li style="display: none;" class="">
+				<a href="http://zsegw.pl/index.php?option=com_content&amp;view=article&amp;id=512&amp;Itemid=175" target="_blank"><img src="http://zsegw.pl/images/slider/technikOZE.jpg" alt="image1" style="width: 600px; height: 376px;"></a>
+				<div class="sb-description">
+					<h3>Technik OZE</h3>
+				</div>
+			</li>
+			<li style="display: none;" class="">
+				<a href="http://zsegw.pl/index.php?option=com_content&amp;view=article&amp;id=578&amp;Itemid=179" target="_blank"><img src="http://zsegw.pl/images/slider/monterelektronik.png" alt="image1" style="width: 600px; height: 376px;"></a>
+				<div class="sb-description">
+					<h3>Elektronik</h3>
+				</div>
+			</li>
+			<li style="display: block;" class="sb-current">
+				<a href="http://zsegw.pl/index.php?option=com_content&amp;view=article&amp;id=1069&amp;Itemid=231" target="_blank"><img src="http://zsegw.pl/images/slider/technikRobotyk.jpg" alt="image1" style="width: 600px; height: 376px;"></a>
+				<div class="sb-description">
+					<h3>Technik robotyk</h3>
+				</div>
+			</li>
+		</ul>
+		<div id="shadow" class="shadow" style="display: block;"></div><div id="nav-arrows" class="nav-arrows" style="display: block;">
+			<a href="#">Next</a>
+			<a href="#">Previous</a>
+		</div>
+		<div id="nav-dots" class="nav-dots" style="display: block;">
+			<span class=""></span>
+			<span class=""></span>
+			<span class=""></span>
+			<span class=""></span>
+			<span class=""></span>
+			<span class="nav-dot-current"></span>
+			<span></span>
+		</div>
+	</div>
+</section>
+<jdoc:include type="modules" name="info" /> 
+<section id="info" class="hdr">
+	<h3>Ważne informacje</h3>
+	<ul>
+	  <li><b> &gt;&gt; Instrukcja uzyskania dostępu do dziennika elektronicznego - Librus &lt;&lt; </b></li>
+	  <li><b> &gt;&gt; UWAGA!!! PLAN LEKCJI NA WTOREK !!!  &lt;&lt; </b></li>
+	</ul>
+	</section>
+<main>
+  <h2>Aktualności</h2>
+  <p>Przykładowy akapit strony.</p>
+
+
+  <jdoc:include type="modules" name="articles" /> 
+  <article>
+	<header>
+	  <h3>UROCZYSTE ROZPOCZĘCIE ROKU SZKOLNEGO</h3>
+	  <p>
+		<a href="/index.php?view=article&amp;catid=69:rok-szkolny-20222023&amp;id=1170:uroczyste-rozpoczcie-roku-szkolnego&amp;format=pdf" title="PDF" onclick="window.open(this.href,'win2','status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no'); return false;" rel="nofollow"><img src="/images/M_images/pdf_button.png" alt="PDF"></a>
+		<a href="/index.php?view=article&amp;catid=69:rok-szkolny-20222023&amp;id=1170:uroczyste-rozpoczcie-roku-szkolnego&amp;tmpl=component&amp;print=1&amp;layout=default&amp;page=" title="Drukuj" onclick="window.open(this.href,'win2','status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no'); return false;" rel="nofollow"><img src="/images/M_images/printButton.png" alt="Drukuj"></a>
+		<a href="/index.php?option=com_mailto&amp;tmpl=component&amp;link=aHR0cDovL3pzZWd3LnBsL2luZGV4LnBocD9vcHRpb249Y29tX2NvbnRlbnQmdmlldz1hcnRpY2xlJmlkPTExNzA6dXJvY3p5c3RlLXJvenBvY3pjaWUtcm9rdS1zemtvbG5lZ28mY2F0aWQ9Njk6cm9rLXN6a29sbnktMjAyMjIwMjMmSXRlbWlkPTEzNA==" title="Email" onclick="window.open(this.href,'win2','width=400,height=350,menubar=yes,resizable=yes'); return false;"><img src="/images/M_images/emailButton.png" alt="Email"></a>
+	  </p>
+	  <p class="small">Wpisany przez Administrator</p>
+	</header>
+	<p style="text-align: justify;" mce_style="text-align: justify;">1 września o&nbsp; godz. 8.30 na szkolnym boisku Dyrektor szkoły Stanisław Jodko serdecznie powitał uczniów klas I, którzy rozpoczynają naukę w 11 oddziałach- 9 &nbsp;klas technikum <br> i 3 klasy szkoły branżowej.</p>
+	<p style="text-align: justify;" mce_style="text-align: justify;">Przedstawił wychowawców klas, powitał grono pedagogiczne i wyraził nadzieję, że ten rok będzie dla wszystkich bogaty w sukcesy naukowe i zawodowe.</p>
+	<p style="text-align: justify;" mce_style="text-align: justify;">Po przywitaniu odbyło się uroczyste ślubowanie klas I. Przedstawiciel każdej z nich wystąpił przed sztandar szkoły i w imieniu pozostałych uczniów złożył ślubowanie.</p>
+	<p style="text-align: justify;" mce_style="text-align: justify;">Po zakończeniu uroczystości uczniowie rozeszli się wraz z wychowawcami do gabinetów, gdzie zostali zapoznani&nbsp; ze strukturą szkoły, planem lekcji, zasadami i innymi istotnymi kwestiami dotyczącymi początku nauki w Zespole Szkół Elektrycznych.</p>
+	<p style="text-align: justify;" mce_style="text-align: justify;">Klasy programowo wyższe miały spotkania z wychowawcami w wyznaczonych gabinetach.</p><p style="text-align: center;" mce_style="text-align: center;">
+	<b>Wszystkim uczniom, nauczycielom i obsłudze administracyjnej szkoły życzymy powodzenia <br> w nowym Roku Szkolnym 2022/2023!!!</b></p><p style="text-align: center;" mce_style="text-align: center;"><br></p><h4 style="text-align: center;" mce_style="text-align: center;"><b><a href="https://zsegwpl-my.sharepoint.com/:f:/g/personal/admin_zsegw_pl/EkQzt41gMtFKiFs9zJAWidcBSozrELh7J4_aji7i4_gb5Q?e=QFguCe" mce_href="https://zsegwpl-my.sharepoint.com/:f:/g/personal/admin_zsegw_pl/EkQzt41gMtFKiFs9zJAWidcBSozrELh7J4_aji7i4_gb5Q?e=QFguCe" target="_blank"><span style="font-size: large;" mce_style="font-size: large;"><b>Galeria - Rozpoczęcie Roku Szkolnego 2022/2023</b></span></a></b></h4><p><br></p><p><b><span style="font-size: large;" mce_style="font-size: large;"><b><br></b></span></b></p></td>
+  </article>
+
+</main>
+<section>
+Partnerzy
+</section>
+
+<jdoc:include type="modules" name="footer" /> 
+<footer>
+	<section>
+	  <h3>Przydatne linki</h3>
+	  <ul>
+		<li><a href="https://pl-pl.facebook.com/people/Zesp%C3%B3%C5%82-Szk%C3%B3%C5%82-Elektrycznych-w-Gorzowie-Wielkopolskim/100057278206520/">Facebook ZSE</a></li>
+		<li><a href="http://internat.zsegw.pl/">Internat</a></li>
+		<li><a href="https://zsel.edupage.org/timetable/">Plan lekcji</a></li>
+		<li><a href="https://aplikacje.edukacja.gorzow.pl">Dziennki elektroniczny</a></li>
+		<li><a href="https://zselgorzow.bip.gov.pl/">Biuletyn informacji publicznej</a></li>
+	  </ul>
+	</section>
+	<section>
+	  <h3>Adres korespondencyjny</h3>
+	  <ul>
+		<li>Zespół Szkół Elektrycznych<br/>
+			ul. Dąbrowskiego 33<br/>
+			66-400 Gorzów Wielkopolski
+		</li>
+		<li>tel.(95) 722 36 39<br/>
+			fax.(95) 722 59 41<br/>
+			Laboratoria: tel.(95) 722 55 72</li>
+		<li>sekretariat@elektryk.gorzow.pl</li>
+	  </ul>
+	</section>
+	<section>
+	  <h3>Placówki ZSE</h3>
+	  <ul>
+		<li><p>Zespół Szkół Elektrycznych</p><p>ul. Dąbrowskiego 33</p></li>
+		<li><p>Pracownie i Laboratoria</p><p>ul. Grobla 9</p></li>
+		<li><p>Budynek dydaktyczny Gim#4</p><p>ul. Grobla 68a</p></li>
+		<li><p>Internat</p><p>ul. Piłsudskiego 9</p></li>
+	  </ul>
+	</section>
+	<section>
+	  <div class="map">
+		<img src="images/mapa-zse.png" alt="Mapa OSM: ZSE GW, ul. Dąbrowskiego 33" />
+	  </div>
+	</section>
+</footer>
+  <script src="js/script.js"></script>
+</body>
+</html>
